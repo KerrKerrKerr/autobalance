8ed3c9485022bab5bf0654e556579dc3878dd4bc
KerrKerrKerr <ustinov2005@gmail.com>
2022-10-17 09:48:25 +0700

    4

--- Новый текстовый документ (2).py
+++ Новый текстовый документ (2).py
@@ -1,5 +1,67 @@
 #from dfg import main
 n = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+list2 = [0,"H","He","Li","Be","B","C","N","O","F","Ne","Na","Mg","Al","Si","P","S","Cl",'Ar','K','Ca','Sc','Ti','V','Cr','Mn','Fe']  
+def second_check(x):
+    d={}
+    while len(x)!=0:
+        a=x[0]
+        print(a)
+        if a in list2:
+            try:
+                d[a]=d[a]+1
+            except:
+                d[a]=1
+        else:
+            a=a+' '
+            a=list(a)
+            if a[1]not in n:
+                a[0]=a[0]+a[1]
+            if (a[1] in n) and (a[2] not in n):
+                try:
+                    d[a[0]]=d[a[0]]+int(a[1])
+                except:
+                    d[a[0]]=int(a[1])
+            elif a[1] in n and a[2] in n:
+                try:
+                    d[a[0]]=d[a[0]]+int(a[1]+a[2])
+                except:
+                    d[a[0]]=int(a[1]+a[2])
+            elif (a[2] in n) and (a[3] not in n):
+                try:
+                    d[a[0]]=d[a[0]]+int(a[2])
+                except:
+                    d[a[0]]=int(a[2])
+            elif a[2] in n and a[3] in n:
+                try:
+                    d[a[0]]=d[a[0]]+int(a[2]+a[3])
+                except:
+                    d[a[0]]=int(a[2]+a[3])
+        x.pop(0)
+    return d
+
+
+x=str(input())
+x=x.split('=>')
+x1,x2=x[0],x[1]
+x1,x2=x1.split('+'),x2.split('+')
+def smt(x):
+    d=[]
+    p1=[]
+    while len(x)!=0:
+        x0=x[0]
+        if x0[0] not in n:
+            p=[1]
+        else:
+            p=[]
+            while len(x0)!=0:
+                if x0[0]in n:
+                    p.append(str(x0[0]))
+                    x0=x0[1:len(x0)]
+                else:
+                    p1.append(p[0:len(p)])
+                    break
+        d.append(x0)
+        x.pop(0)
 
 def main(input):
     input = input.split('=>')
